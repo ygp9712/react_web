b14aec4f208c5ebda812114b321560aed7b6b82d
杨国鹏 <yangguopeng@ttg.cn>
2022-02-16 16:14:52 +0800

    '修改'

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,18 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import styles from './App.module.css';
+import { Layout, Typography, Input } from "antd"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.App}>
+      <div>
+        <Layout.Header>
+          <img src={logo} />
+          <Typography.Title level={3}>React旅游网</Typography.Title>
+          <Input.Search placeholder='请输入旅游目的地、主题或关键字' />
+        </Layout.Header>
+      </div>
     </div>
   );
 }
---
+++ src/custom.d.ts
@@ -0,0 +1,4 @@
+declare module "*.css" { // 对css文件的声明
+  const css : { [key: string]: string };
+  export default css;
+}
